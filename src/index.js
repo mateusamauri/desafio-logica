8d54aab692912c744db8fe9ee61e50ba24cc184e
mateusamauri <mateusamauri25@gmail.com>
2024-05-02 14:58:58 -0300

    Entrega do desafio

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
-console.log("ola mundo")
+let nomeHeroi = "Batman"
+let xpHeroi = 6.350
+
+
+if(xpHeroi < 1.000){
+
+    console.log("Ferro")
+
+}else if(xpHeroi > 1.000 && xpHeroi <= 2.000){
+
+    console.log("Bronze")
+
+}else if(xpHeroi > 2.000 && xpHeroi <= 5.000){
+
+    console.log("Prata")
+
+}else if(xpHeroi > 5.000 && xpHeroi <= 7.000){
+
+    console.log("Ouro")
+    
+}else if(xpHeroi > 7.000 && xpHeroi <= 8.000){
+
+    console.log("Platina")
+
+}else if(xpHeroi > 8.000 && xpHeroi <= 9.000){
+
+    console.log("Ascendente")
+
+}else if(xpHeroi > 9.000 && xpHeroi <= 10.000){
+
+    console.log("Imortal")
+
+}else{
+    console.log("Radiante")
+}
